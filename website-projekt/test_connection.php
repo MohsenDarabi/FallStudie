6e7b9e31a6af2f8a47a29522400b41fc2b045bd8
Mohsen Darabi <mohsen.darabi@outlook.de>
2024-03-13 14:25:19 +0100

    change test_connection.php change the code to show if there is some problem with the connection, if not print "Connected successfully to the database! "
    at the beginning add error reporting functionality
    to the php page

--- website-projekt/test_connection.php
+++ website-projekt/test_connection.php
@@ -1,9 +1,13 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Database configuration
 $servername = "localhost";
 $username = "root"; // Default username for XAMPP MySQL
 $password = ""; // Default password for XAMPP MySQL (empty by default)
-$dbname = "my_blog_db"; // My database name
+$dbname = "my_blog_db"; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,5 +15,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database!";
 }
+
+// Close connection
+$conn->close();
 ?>
